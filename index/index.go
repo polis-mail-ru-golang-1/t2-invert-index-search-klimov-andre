c1f8666442094d3f10e934d3f31e8fb0bb2c1492
Андрей <klimandr1998@gmail.com>
2018-10-29 21:43:46 +0300

    Добавлена многопоточность

--- index/index.go
+++ index/index.go
@@ -1,9 +1,13 @@
 package index
 
 import (
+	"io/ioutil"
 	"sort"
 	"strings"
+	"sync"
 )
+
+type Database map[string]Index
 
 //Index хранит слово и структуру extFiles, относящуюся к нему
 type Index struct {
@@ -11,25 +15,75 @@ type Index struct {
 	Files []ExtFiles
 }
 
-//ExtFiles хранит имя файла и вес этого файла
+// ExtFiles хранит имя файла и вес этого файла
 //(для каждого слова - количество встреч в данном файле)
 type ExtFiles struct {
 	Filename string
 	Weight   int
 }
 
-//ResStruct хранит уже обработанные данные
+// ResStruct хранит уже обработанные данные
 type ResStruct struct {
 	Filename string
 	Weight   int
 }
 
-//FileIndexing обновляет стркутуру обратного индекса в файле filename
-func FileIndexing(arrayIndexes map[string]Index, inputContent []byte, filename string) {
+// MergeIt копит результаты работы всех тредов в одну базу
+func MergeIt(database Database, chWork chan Index, chError chan error, filesCnt int, waiter *sync.WaitGroup) {
+	defer waiter.Done()
+	successCnt := 0
+MergeLoop:
+	for {
+		select {
+		case err := <-chError:
+			if err == nil {
+				successCnt++
+				if successCnt == filesCnt {
+					break MergeLoop
+				}
+			}
+		case val := <-chWork:
+			_, ok := database[val.Word]
+			if ok {
+				index := database[val.Word]
+				index.Files = append(index.Files, val.Files[0])
+				database[val.Word] = index
+			} else {
+				database[val.Word] = val
+			}
+		}
+	}
+}
+
+// ReadIt читает файл и создает базу по нему
+func ReadIt(filename string, ch chan Index, chError chan error) {
+	myDatabase := make(Database, 1)
+	fileBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		chError <- err
+	} else {
+		FileIndexing(myDatabase, fileBytes, filename)
+		for _, val := range myDatabase {
+			ch <- val
+		}
+		chError <- nil
+	}
+}
+
+func cleanWord(in string) string {
+	in = strings.ToLower(in)
+	in = strings.TrimFunc(in, func(c rune) bool {
+		return (c < 48 || c > 57) && (c < 97 || c > 122) && c != 45
+	})
+	return string(in)
+}
+
+// FileIndexing обновляет стркутуру обратного индекса в файле filename
+func FileIndexing(arrayIndexes Database, inputContent []byte, filename string) {
 	str := string(inputContent)
-	words := strings.Split(str, " ")
+	words := strings.Fields(str)
 	for i := 0; i < len(words); i++ {
-		word := words[i]
+		word := cleanWord(words[i])
 		_, ok := arrayIndexes[word]
 		if !ok {
 			newWordIdx := Index{Word: word}
@@ -42,7 +96,9 @@ func FileIndexing(arrayIndexes map[string]Index, inputContent []byte, filename s
 				if arrayIndexes[word].Files[j].Filename == filename {
 					arrayIndexes[word].Files[j].Weight++
 					isExist = true
-					sort.SliceStable(arrayIndexes[word].Files, func(i, j int) bool { return arrayIndexes[word].Files[i].Weight > arrayIndexes[word].Files[j].Weight })
+					sort.SliceStable(arrayIndexes[word].Files, func(i, j int) bool {
+						return arrayIndexes[word].Files[i].Weight > arrayIndexes[word].Files[j].Weight
+					})
 				}
 			}
 			if !isExist {
@@ -51,20 +107,19 @@ func FileIndexing(arrayIndexes map[string]Index, inputContent []byte, filename s
 				tmp.Files = append(tmp.Files, newFile)
 				arrayIndexes[word] = tmp
 			}
-
 		}
-
 	}
 }
 
 // GetResults обрабатывает базу данных и возвращает структуру результата
-func GetResults(arrayIndexes map[string]Index, userTokens []string) []ResStruct {
+func GetResults(arrayIndexes Database, userTokens []string) []ResStruct {
 	var resultIdx []ResStruct
 	for i := 0; i < len(userTokens); i++ {
-		val, ok := arrayIndexes[userTokens[i]]
+		userWord := cleanWord(userTokens[i])
+		val, ok := arrayIndexes[userWord]
 		if ok {
 			for j := 0; j < len(val.Files); j++ {
-				var isExists bool
+				isExists := false
 				for k := 0; k < len(resultIdx); k++ {
 					if resultIdx[k].Filename == val.Files[j].Filename {
 						resultIdx[k].Weight += val.Files[j].Weight
@@ -74,10 +129,12 @@ func GetResults(arrayIndexes map[string]Index, userTokens []string) []ResStruct 
 				if !isExists {
 					tmp := ResStruct{val.Files[j].Filename, val.Files[j].Weight}
 					resultIdx = append(resultIdx, tmp)
-					sort.SliceStable(resultIdx, func(i, j int) bool { return resultIdx[i].Weight > resultIdx[j].Weight })
 				}
 			}
 		}
 	}
+	sort.SliceStable(resultIdx, func(i, j int) bool {
+		return resultIdx[i].Weight > resultIdx[j].Weight
+	})
 	return resultIdx
 }
